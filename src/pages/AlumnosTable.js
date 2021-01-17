e03d2257319f8f192fd85cffbf884b64db3dd324
Aarón Velasco <aaronvdlp@gmail.com>
2021-01-17 17:57:54 +0100

    feat: :sparkles: Chequeo de que el usuario esté logeado en la pagina inicial

--- src/pages/AlumnosTable.js
+++ src/pages/AlumnosTable.js
@@ -5,25 +5,14 @@ import { Link } from "react-router-dom";
 function AlumnosTable() {
   const [alumnos, setAlumnos] = useState([]);
   const [loading, setLoading] = useState("Cargando");
-  var menuHead = "";
-  if(localStorage.getItem('logged_in')){
-    fetch(`${process.env.REACT_APP_API_URL}/auth/user`,{
-      method: 'get',
-      headers: {
-        "Content-type": "application/json", 
-        "Authorization" : `Bearer ${localStorage.getItem('api_token')}`
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => (data))
-        .catch((error) => localStorage.setItem('logged_in',false))
-    menuHead = <div>
-        <Link className="text-blue-600 underline" to="/alumno/create"> Crear alumno </Link>
-    </div>
-  } else {
-      menuHead = <Link className="text-blue-600 underline" to="/login"> Iniciar sesión </Link>
-  }
+  const [menuHead, setMenuHead] = useState("");
+
   useEffect(() => {
+    if(localStorage.getItem('logged_in') !== true){
+      setMenuHead(<Link className="text-blue-600 underline" to="/login">Iniciar sesión</Link>)
+    } else {
+      setMenuHead(<Link className="text-blue-600 underline" to="/alumno/create">Crear alumno</Link>)
+    }
     fetch(`${process.env.REACT_APP_API_URL}/alumnos`)
       .then((response) => response.json())
       .then((data) =>
@@ -32,14 +21,15 @@ function AlumnosTable() {
             <AlumnosTableRow key={alumno.uuid} alumno={alumno} />
           )),
           setLoading("")
-        ))
-        .catch((error) => setLoading('Error al conectarse con la API'))
-  },[]);
+        )
+      )
+      .catch((error) => setLoading("Error al conectarse con la API"));
+  }, []);
 
   return (
     <div className="bg-white rounded-lg shadow-md lg:shadow-lg h-screen">
       <h1 className="py-12 text-4xl">Listado de alumnos</h1>
-        {menuHead}
+      {menuHead}
       <br />
       <table className="mx-auto m-4 w-4/6 table-auto border-collapse border border-green-800">
         <thead className="shadow-md">
