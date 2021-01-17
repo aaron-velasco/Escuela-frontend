a53e803c5be901450f19a09caf04dc3e59909132
Aarón Velasco <aaronvdlp@gmail.com>
2021-01-17 14:20:26 +0100

    feat: :sparkles: Cambiado layout rows de tabla de alumnos

--- src/components/AlumnosTableRow.js
+++ src/components/AlumnosTableRow.js
@@ -4,17 +4,14 @@ import {Link} from "react-router-dom"
 function AlumnosTableRow(props) {
     let edit = ''
     if(localStorage.getItem('logged_in'))
-         edit = <Link to={ `/alumno/edit/${props.alumno.uuid}` } className="text-blue-600 underline">Editar</Link>
+         edit = <Link to={ `/alumno/edit/${props.alumno.uuid}` } className="text-yellow-600 underline">Editar</Link>
     return (
         <tr>
-            <td>{props.alumno.uuid}</td>
-            <td>{props.alumno.nombre}</td>
-            <td>{props.alumno.apellidos}</td>
-            <td>{props.alumno.direccion}</td>
-            <td>{props.alumno.poblacion}</td>
-            <td>{props.alumno.codigo_postal}</td>
-            <td>{props.alumno.curso}</td>
-            <td><Link to={ `/alumno/${props.alumno.uuid}` } className="text-blue-600 underline">Ver</Link> {edit}</td>
+            <td className="px-16 py-2">{props.alumno.uuid}</td>
+            <td className="px-16 py-2">{props.alumno.nombre}</td>
+            <td className="px-16 py-2">{props.alumno.apellidos}</td>
+            <td className="px-16 py-2">{props.alumno.curso}</td>
+            <td className="px-16 py-2"><Link to={ `/alumno/${props.alumno.uuid}` } className="text-blue-600 underline">Ver</Link> {edit}</td>
         </tr>
     )
 }
