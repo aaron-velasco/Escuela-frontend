932d0363a4c898dc41ee9849b54b172033e752d6
Aarón Velasco <aaronvdlp@gmail.com>
2021-01-17 17:57:14 +0100

    fix: :bug: Fix mostrar menu editar al estar logeado

--- src/components/AlumnosTableRow.js
+++ src/components/AlumnosTableRow.js
@@ -1,17 +1,21 @@
-import React from "react"
+import {React, useState, useEffect} from "react"
 import {Link} from "react-router-dom"
 
 function AlumnosTableRow(props) {
-    let edit = ''
-    if(localStorage.getItem('logged_in'))
-         edit = <Link to={ `/alumno/${props.alumno.uuid}/edit/` } className="text-yellow-600 underline">Editar</Link>
+    const [editMenu, setEditMenu] = useState("");
+
+    useEffect(() => {
+        if(localStorage.getItem('logged_in') === true){
+        setEditMenu(<Link to={ `/alumno/${props.alumno.uuid}/edit/` } className="text-yellow-600 underline">Editar</Link>)}
+    },[])
+   
     return (
         <tr className="shadow-sm">
             <td className="px-16 py-2">{props.alumno.uuid}</td>
             <td className="px-16 py-2">{props.alumno.nombre}</td>
             <td className="px-16 py-2">{props.alumno.apellidos}</td>
             <td className="px-16 py-2">{props.alumno.curso}</td>
-            <td className="px-16 py-2"><Link to={ `/alumno/${props.alumno.uuid}` } className="text-blue-600 underline">Ver</Link> {edit}</td>
+            <td className="px-16 py-2"><Link to={ `/alumno/${props.alumno.uuid}` } className="text-blue-600 underline">Ver</Link> {editMenu}</td>
         </tr>
     )
 }
