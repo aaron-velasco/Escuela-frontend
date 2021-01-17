1d5a9d0151fa3cbb2fe02ebf1a726a6b8a83b2e3
Aarón Velasco <aaronvdlp@gmail.com>
2021-01-17 13:48:26 +0100

    feat: :sparkles: Integración react router y habilitadas rutas de login y de ver alumno

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import './tailwind.css';
+import Login from './components/Login'
 import AlumnosTable from './components/AlumnosTable'
-
+import Alumno from './components/Alumno'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App container mx-auto">
-      <AlumnosTable />
-    </div>
+    <Router>
+      <div className="App container mx-auto">
+      <Switch>
+          <Route path="/login"  component={Login} />
+          <Route path="/alumno/:id"  component={Alumno} />
+          <Route exact path="/" component={AlumnosTable} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
